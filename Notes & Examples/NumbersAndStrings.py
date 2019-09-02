43ec72ee2d5f73efe6c365f2db824ce37fd488ab
unknown <devethancraig@gmail.com>
2019-09-02 19:00:00 +0100

    First Hour of Lectures

---
+++ Notes & Examples/NumbersAndStrings.py
@@ -0,0 +1,85 @@
+# Mod Operator
+7%4 
+
+#Powers
+2 ** 8
+
+(2 + 10) * (10+3)
+
+#Var
+a = 10
+
+a = a + a
+
+type(a)
+
+my_income = 100
+
+tax_rate = 0.1
+
+my_taxes = my_income * tax_rate
+
+#Strings
+print('Hello World One.')
+print('Hello World Two.')
+
+#Esc Sequence
+print('Hello \nworld')
+#Tab Sequence
+print('Hello \tworld')
+
+#Length
+len("I am a String")
+
+#Indexing
+myString = "Hello World"
+myString[0]
+myString[8]
+myString[-2]
+
+#Slicing
+myString = 'abcdefghijkl'
+
+myString[2:] 
+myString[:3]
+myString[3:6]
+myString[::3]
+myString[::-1]
+
+#Properties & Methods
+name = 'Ethan'
+
+last_letters = name[2:]
+
+#Concatanations
+"Nat" + last_letters
+
+letter = 'z'
+
+letter * 10
+
+"3" + "2"
+
+#Methods 
+x = "Hello World"
+
+x.upper()
+x.lower()
+x.split()
+
+#Print Formatting Strings
+print('This is a string {}'.format('INSERTED'))
+
+print('The {2} {1} {0}'.format('fox', 'brown', 'quick'))
+
+print('The {q} {b} {f}'.format(f='fox', b='brown', q='quick'))
+
+#Float Formatting
+result = 100/777
+
+print('The Result Was {r:1.3f}'.format(r=result))
+
+#Ver. 3.6
+name = "Jose"
+
+print(f'Hello, his name is {name}')
